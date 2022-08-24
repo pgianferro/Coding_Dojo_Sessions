7280a87feca14e8de22063940be76e09bceb4e19
Pablo Gianferro <pablogianferro@gmail.com>
2022-08-24 20:11:26 +0000

    Added the conditional for isNaN

--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@
 function addPG(string) {
   let array = string.split(",");
   let arrOfNum = array.map((strings) => {
-    return Number(strings);
+    if (isNaN(strings)) {
+      return 0;
+    } else {return Number(strings)}
   });
   let sum = arrOfNum.reduce((partialSum, a) => partialSum + a, 0);
   return String(sum);
 }
 
-console.log(addPG("0.5,49.5,-50,1000"));
+console.log(addPG("a,49.5,-50,100,Vamos Racing"));
+console.log(addPG("Racing tiene, 45, puntos"));
