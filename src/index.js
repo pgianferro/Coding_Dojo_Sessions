7c7c9dde5eefed68343888507fc4bc9d2a74697c
Pablo Gianferro <pablogianferro@gmail.com>
2022-08-24 20:13:19 +0000

    Adding Chellenge sentence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+//CHALLENGE
+// String Calculator
+// Create a function add that takes a String and returns a String:
+
+// String add(String number)
+// The method can take n separated by comma, and returns their sum.
+// An empty string will return “0”.
+// Example of inputs: "", "1", "1.1,2.2".
+// Negative numbers should be allowed
+// Don’t allow the input to end in a separator.
+
+//SOLUTION
 //A variant with only string methods
 
 //Steps
